49a48de18e3686a09bfc104ad11d471fbd5e45e2
Seagulll <drouniee@gmail.com>
2025-09-28 19:39:34 +0300

    2 лабораторная: Первое задание; Второе задание наполовину

---
+++ src/ru/mirea/lab2/TestAuthor.java
@@ -0,0 +1,17 @@
+package ru.mirea.lab2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author author = new Author("Druzhnov Maksim", "druzhnov.m.v@edu.mirea.ru", 'M');
+
+        System.out.println("Имя: " + author.getName());
+        System.out.println("Почта: " + author.getEmail());
+        System.out.println("Пол: " + author.getGender());
+
+        author.setEmail("drouniee@gmail.com");
+        System.out.println("Новая почта: " + author.getEmail());
+
+        System.out.println(author.toString());
+
+    }
+}
