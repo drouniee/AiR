426887966d9b15f37e0bbe41fd44e4647a16ca9b
Seagulll <drouniee@gmail.com>
2025-12-15 20:54:51 +0300

    Конец 2

--- src/ru/mirea/lab20/MinMaxTest.java
+++ src/ru/mirea/lab20/MinMaxTest.java
@@ -3,6 +3,10 @@ package ru.mirea.lab20;
 public class MinMaxTest {
     public static void main(String[] args) {
         Integer[] numbers = {5, 2, 8, 1, 9};
+        for (int num : numbers){
+            System.out.print(num + " ");
+        }
+        System.out.println();
         MinMax<Integer> minMax = new MinMax<>(numbers);
         System.out.println("Min: " + minMax.findMin());
         System.out.println("Max: " + minMax.findMax());
